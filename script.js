5a8bb006f89d4901e4649439f368eaf069ba2050
gillvasoff <gillvasoff@gmail.com>
2023-03-09 09:03:29 -0500

    Construct the grid and implement basic drawing logic

---
+++ script.js
@@ -0,0 +1,38 @@
+function toggleDraw() {
+    drawOn = !drawOn;
+    if (drawOn) 
+    {
+        this.style.backgroundColor = "black";
+    }
+}
+
+function draw(){
+    if (drawOn) 
+    {
+        this.style.backgroundColor = "black";
+    }
+}
+
+let drawOn = false;
+let penColor = 0x000000;
+const divArray = [];
+numDiv = 256;
+
+for (let i =0; i < numDiv; i++)
+{
+    let div = document.createElement('div');
+    div.className = "cells";
+    div.style.minHeight = "38px"
+    div.style.width = "38px";
+    div.style.border = "1px solid black";
+    div.style.margin = "0px";
+    div.style.padding = "0px";
+
+    divArray.push(div);
+
+    let container = document.querySelector('.divContainer');
+    container.appendChild(div);
+}
+
+divArray.forEach(element => {element.addEventListener("click", toggleDraw)}); 
+divArray.forEach(element => {element.addEventListener("mouseover", draw)})
